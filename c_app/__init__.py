38a53224bd799ca6b12d29af983d18580be01be6
nyaribari <anyaribari@gmail.com>
2022-05-03 12:38:22 +0300

    create a filter to format a currency based on the current local language

--- c_app/__init__.py
+++ c_app/__init__.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from locale import currency
+import ccy
+from flask import Flask, request
 from c_app.product.views import fashion
 
 app = Flask(__name__)
 app.register_blueprint(fashion)
+
+@app.template_filter('format_currency')
+def format_currency_filter(amount):
+    currency_code = ccy.countryccy(request.accept_languages.best[-2:])
+    return f'{currency_code}  {amount}'
